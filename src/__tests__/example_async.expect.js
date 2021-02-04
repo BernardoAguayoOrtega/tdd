8547a2d3086e1404fa75d0c3b764c85b1f189e00
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-04 13:40:20 -0600

    [add] async await code

--- src/__tests__/example_async.expect.js
+++ src/__tests__/example_async.expect.js
@@ -1,14 +1,26 @@
 const asyncCallback = (cb) => {
-  setTimeout(() => {
-    cb(true);
-  }, 1000)
-}
+	setTimeout(() => {
+		cb(true);
+	}, 1000);
+};
+
+const promise = new Promise((resolve, reject) => resolve(true));
 
 describe('async code', () => {
-  it('example of async with callback', (done) => {
-    asyncCallback(cb => {
-      expect(cb).toBe(true);
-      done()
-    })
-  })
-})
+	it('example of async with callback', (done) => {
+		asyncCallback((cb) => {
+			expect(cb).toBe(true);
+			done();
+		});
+	});
+
+	it('example of promise', () => {
+		return promise.then((result) => expect(result).toBe(true));
+	});
+
+	it('example of promise 2', async() => {
+    const result = await promise
+    
+    expect(result).toBe(true)
+	});
+});
