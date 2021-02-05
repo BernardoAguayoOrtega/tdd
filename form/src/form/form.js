002fc30f1238bb193958c2ce8396c23cc8da0005
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-02-05 08:58:51 -0600

    [add] add test to check if we have the react structure

--- form/src/form/form.js
+++ form/src/form/form.js
@@ -1,3 +1,30 @@
-const Form = () => <h1>create product</h1>
+import { InputLabel, Select } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
+
+const Form = () => (
+  <>
+    <h1>create product</h1>
+    <form>
+      <TextField label="name" id="name"/>
+      <TextField label="size" id="size"/>
+
+    <InputLabel htmlFor="type">Type</InputLabel>
+
+      <Select
+        native
+        value=""
+        inputProps={{
+          name: "type",
+          id: "type",
+        }}
+        >
+          <option aria-label="None" value="" />
+          <option value="electronic">electronic</option>
+          <option value="furniture">furniture</option>
+          <option value="clothing">clothing</option>
+        </Select>
+    </form>
+  </>
+)
 
 export default Form
--- form/src/form/form.test.js
+++ form/src/form/form.test.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { screen, render} from '@testing-library/react'
+import {screen, render} from '@testing-library/react'
 
 import Form from './form'
 
 describe('when te form is mounted', () => {
-  it('there must be a create product form page', () => {
-    render(<Form />)
+  beforeEach(() => render(<Form />))
 
-    expect(screen.getByRole('heading', {name: /create product/i})).toBeInTheDocument()
+  it('there must be a create product form page', () => {
+    expect(
+      screen.getByRole('heading', {name: /create product/i}),
+    ).toBeInTheDocument()
+  })
+
+  it('should exists fiedls: name, size, type (electronic, furniture, clothing)', () => {
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/size/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/type/i)).toBeInTheDocument()
+
+    expect(screen.queryByText(/electronic/i)).toBeInTheDocument()
+    expect(screen.queryByText(/furniture/i)).toBeInTheDocument()
+    expect(screen.queryByText(/clothing/i)).toBeInTheDocument()
   })
 })
